dfd915fc312935dd22b294b8653001c6fac188e1
yong du <yongdu90@gmail.com>
2018-06-15 23:54:37 +0200

    set up outline

---
+++ Algorithm/HashTables.java
@@ -0,0 +1,4 @@
+/*
+*  Search algortithms that use hashing consisting of two separate parts.
+*  1st part: compute a hash function that transform the search key into an array index 
+*/
---
+++ Review/2018w24_DP_VS_DC.md
@@ -0,0 +1,4 @@
+# This is a review task for [Dynamic Programming vs Divide-and-Conquer][DPvsDC]
+
+
+[DPvsDC]: https://itnext.io/dynamic-programming-vs-divide-and-conquer-2fea680becbe
